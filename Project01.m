b30c89036fbc7c8e54d0056ede008b399cfa77db
Shamit Bagchi <shamit@gmail.com>
2015-12-01 22:24:26 +0100

    Cleaned up code.

--- Project01.m
+++ Project01.m
@@ -6,11 +6,17 @@ u = zeros(NUM_PLAYERS, NUM_PLAYERS);
 a = randi([0 1], NUM_PLAYERS, NUM_PLAYERS);
 c = 1; % cost
 beta = 1;
+max_k = 3;
+
 for i = 1 : NUM_PLAYERS
-    for k = 1 : 3
-        u(i,:) = -c*sum(a(i,:)) + (beta^k)*(a(i,:).^(k) - a(i,:).^(k-1));
+    sumk = 0;
+    for k = 1 : max_k
+        ak = a^(k);
+        ak1 = a^(k-1)
+        sumk = sumk + (beta^k)*(sum(ak(i,:)) - sum(ak1(i,:)));
     end
+    u(i,:) = -c*sum(a(i,:)) + sumk;
 end
 
-
+display(u);
 
