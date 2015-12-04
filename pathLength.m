8200e220fdfca4790cf6adccb4c1949f08045f4f
Jared Karr <jaredkarr@users.noreply.github.com>
2015-12-04 18:41:20 +0100

    Update pathLength.m
    ~4x speed improvement on pathLength calculation

--- pathLength.m
+++ pathLength.m
@@ -2,14 +2,13 @@ function pL = pathLength(A)
 % pL = pathLength(A)
 % expects sparse adjacency matrix A
 
-    pL = full(A);	% path 1 is just A
-    n = size(A, 1);     % get node count
-    depth = 1;          % current path length
-    frontier = A;
-    while depth < n && any(frontier(:)) % until depth or connections are exhausted
+    pL = full(A);	          % path 1 is just A
+    depth = 1;                % current path length
+    frontier = A > 0;
+    while any(frontier(:))    % until connections are exhausted
         depth = depth + 1;
-        frontier = frontier * A;        % take one step
-        frontier(pL~=0) = 0;            % remove duplicates
-        pL(frontier~=0) = depth;        % add to path length matrix
+                              % step forward without duplicates
+        frontier = and(frontier * A > 0, ~pL);
+        pL(frontier) = depth; % add to path length matrix
     end
 end
