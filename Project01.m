69d9ca7d8f79c07e178f9e3ea980461784bd1f89
Bjorn Persmats <bjorn.pm@plankter.se>
2015-12-07 12:49:11 +0100

    Added some todos and initial action

--- Project01.m
+++ Project01.m
@@ -3,17 +3,23 @@ close all;
 % Follower's Game
 
 NUM_PLAYERS = 5;
-A = randi([0 1], NUM_PLAYERS, NUM_PLAYERS);
-A = A.*(eye(NUM_PLAYERS)==0);
+%A = randi([0 1], NUM_PLAYERS, NUM_PLAYERS);
+%A = A.*(eye(NUM_PLAYERS)==0);
+% TODO: initialise A using initialAction()?
+A = initialAction(NUM_PLAYERS, 0.1);
+
 score = zeros(NUM_PLAYERS,1);
 cost = 1; % cost
 beta = 1;
 
 NUM_ITER = 5;
 
+% TODO: Put all strategies (function handles) in a cell array (or smth) 
+
 
 for i = 1 : NUM_ITER
     pL = pathLength(A);
+    % TODO: iterate game using iterateGame()?
     score = utility(A, pL, beta, cost);
 end
 U = score;
