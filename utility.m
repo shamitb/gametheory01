d6362ae55c46fcb0d7caed7e3c1af518fd3c0c22
jaredkarr <jaredkarr@users.noreply.github.com>
2015-12-08 10:46:40 +0100

    Persistent beta and cost

--- utility.m
+++ utility.m
@@ -1,19 +1,30 @@
-function score = utility(A, pL, beta, cost)
+function score = utility(A, pL, beta, cost, temporary)
 % score = utility(A, pL, beta, cost)
 % sparse adjacency matrix A
 % optional precomputed path length matrix pL
 % optional beta, default is 1
 % optional cost, default is 1
+% optional temporary, default is false
 % returns N-by-1 vector
 
-    if ~exist('pL','var') || isempty(pL)       % compute pL if needed
+    persistent b
+    persistent c
+    if isempty(b)
+        b = 1; c = 1;
+    end
+
+    if ~exist('pL','var') || isempty(pL)        % compute pL if needed
         pL=pathLength(A);
     end
-    if ~exist('beta','var') || ~isreal(beta)   % default beta
-        beta = 1;
+    if ~exist('beta','var') || ~isreal(beta)    % default beta
+        beta = b;
     end
-    if ~exist('cost','var') || ~isreal(cost)   % default cost
-        cost = 1;
+    if ~exist('cost','var') || ~isreal(cost)    % default cost
+        cost = c;
+    end
+    if ~exist('temporary', 'var') || ~temporary % save beta and cost by default
+        b = beta;
+        c = cost;
     end
     
     score = sum(beta.^pL.*(pL~=0))' - cost * sum(A, 2);
