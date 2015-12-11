cb6040e544d64c9906108bdbb3f55f5dd92971ca
Shamit Bagchi <shamit@gmail.com>
2015-12-11 23:01:02 +0100

    Cumulative Trials

--- testaltruistic.m
+++ testaltruistic.m
@@ -3,29 +3,40 @@ function testaltruistic()
 clear all;
 
 N = 10;
-A = initialAction(N, 4/N);
+    
+maxTrials = 30;
+Ucummulative = zeros(1, N);
 
-beta = 1;
-cost = 0.5;
+for i= 1:maxTrials
 
+    A = initialAction(N, 4/N);
+    
+    beta = 1;
+    cost = 0.5;
 
-pL = pathLength(A);
-U = utility(A, pL, beta, cost);
+    pL = pathLength(A);
+    U = utility(A, pL, beta, cost);
+    
+    Uprev = U';
+    
+%     figure(1);
+%     plot(digraph(A))
+    
+    handle = generateStrategyAltruist(beta, cost);
+    [connection A, pL, U] = handle(1, A, pL, U);
+    
+%     figure(2);
+%     plot(digraph(A))
+    
+    Unext = U';
+    
+    Udiff = Unext - Uprev
+    
+    Ucummulative = Ucummulative + Udiff;
+    
+end
 
-Uprev = U';
-
-figure(1);
-plot(digraph(A))
-
-handle = generateStrategyAltruist(beta, cost);
-[connection A, pL, U] = handle(1, A, pL, U);
-
-figure(2);
-plot(digraph(A))
-
-Unext = U';
-
-Udiff = Unext - Uprev
+UDiffAvg = Ucummulative/maxTrials
 
 % [S, A, U, SHistory, AHistory] = iterateGame(S, A, pL, U, 500, false, strategy, 0.05);
 
