d3a18fa56ed35c1ea1f803dc73446654f4e9c818
Shamit Bagchi <shamit@gmail.com>
2015-12-12 21:13:42 +0100

    Ccompared with brute force method

--- testaltruistic.m
+++ testaltruistic.m
@@ -3,40 +3,52 @@ function testaltruistic()
 clear all;
 
 N = 20;
-    
+
 maxTrials = 30;
 Ucummulative = zeros(1, N);
 
-for i= 1:maxTrials
+A = initialAction(N, 4/N);
 
-    A = initialAction(N, 4/N);
-    
-    beta = 1;
-    cost = 0.5; 
+beta = 1;
+cost = 0.5;
 
-    pL = pathLength(A);
-    U = utility(A, pL, beta, cost);
-    
-    Uprev = U';
-    
+pL = pathLength(A);
+U = utility(A, pL, beta, cost);
+
+sumUprev = sum(U);
+
 %     figure(1);
 %     plot(digraph(A))
-    
-    handle = generateStrategyAltruist(beta, cost);
-    [connection A, pL, U] = handle(1, A, pL, U);
-    
-%     figure(2);
-%     plot(digraph(A))
-    
-    Unext = U';
-    
-    Udiff = Unext - Uprev
-    
-    Ucummulative = Ucummulative + Udiff;
-    
+utilitySet = [];
+for i = 1 : N
+    Atemp = A;
+    Atemp(1, i) = 1;
+    temppL = pathLength(Atemp);
+    U1 = utility(Atemp, temppL, beta, cost);
+    utilitySet = [utilitySet sum(U1)];
 end
 
-UDiffAvg = Ucummulative/maxTrials
+
+handle = generateStrategyAltruist(beta, cost);
+[connection A, pL, U] = handle(1, A, pL, U);
+
+utilitySet = utilitySet - sumUprev
+
+%     figure(2);
+%     plot(digraph(A))
+
+sumUnext = sum(U);
+
+Udiff = sumUnext - sumUprev
+
+
+
+%     for i= 1:maxTrials
+%     Ucummulative = Ucummulative + Udiff;
+%
+% end
+%
+% UDiffAvg = Ucummulative/maxTrials
 
 % [S, A, U, SHistory, AHistory] = iterateGame(S, A, pL, U, 500, false, strategy, 0.05);
 
