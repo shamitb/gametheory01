688bea420ab3ee0bf01530b81fd367362440bbd6
jaredkarr <jaredkarr@users.noreply.github.com>
2015-12-19 21:04:38 +0100

    Change to infinite distance for unrealized paths

--- pathLength.m
+++ pathLength.m
@@ -1,14 +1,14 @@
 function pL = pathLength(A)
 % pL = pathLength(A)
-% expects sparse adjacency matrix A
 
-    pL = full(A);	          % path 1 is just A
+    N = length(A);
+    pL = inf(N);	          % assume infinite distance
     depth = 1;                % current path length
     frontier = A > 0;
     while any(frontier(:))    % until connections are exhausted
+        pL(frontier) = depth;
+                              % step forward into unchecked paths
+        frontier = and(frontier * A > 0, isinf(pL));
         depth = depth + 1;
-                              % step forward without duplicates
-        frontier = and(frontier * A > 0, ~pL);
-        pL(frontier) = depth; % add to path length matrix
     end
 end
--- utility.m
+++ utility.m
@@ -1,32 +1,44 @@
-function score = utility(A, pL, beta, cost, temporary)
+function [U, pL] = utility(A, pL, b, c, temporary)
 % score = utility(A, pL, beta, cost)
-% sparse adjacency matrix A
+% adjacency matrix A (N×N)
 % optional precomputed path length matrix pL
 % optional beta, default is 1
 % optional cost, default is 1
 % optional temporary, default is false
 % returns N-by-1 vector
 
-    persistent b
-    persistent c
-    if isempty(b)
-        b = 1; c = 1;
+    persistent BETA
+    persistent COST
+    
+    if isempty(BETA)                      % initial defaults
+        BETA = 1; COST = 1;
     end
 
-    if ~exist('pL','var') || isempty(pL)        % compute pL if needed
-        pL=pathLength(A);
-    end
-    if ~exist('beta','var') || ~isreal(beta)    % default beta
-        beta = b;
-    end
-    if ~exist('cost','var') || ~isreal(cost)    % default cost
-        cost = c;
-    end
-    if ~exist('temporary', 'var') || ~temporary % save beta and cost by default
-        b = beta;
-        c = cost;
+    if ~exist('b','var') || ~isreal(b)    % default beta
+        b = BETA;
     end
     
-    score = sum(beta.^pL.*(pL~=0))' - cost * sum(A, 2);
+    if ~exist('c','var') || ~isreal(c)    % default cost
+        c = COST;
+    end
+    
+    if ~exist('temporary', 'var') ||...
+       ~temporary                         % save beta and cost by default
+        BETA = b;
+        COST = c;
+    end
+    
+    if ~exist('pL','var') || isempty(pL)  % compute pL if needed
+        pL=pathLength(A);
+    end
+    
+    if b < 1
+        U = sum(b .^ pL)';
+    else
+        N = length(A);
+        U = sum(pL <= N)';
+    end
+    
+    U = U - c * sum(A, 2);
 
 end
