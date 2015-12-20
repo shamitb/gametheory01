614163b291a0a24ad1db4d212dd973af36707219
jaredkarr <jaredkarr@users.noreply.github.com>
2015-12-20 17:01:43 +0100

    Compute from adjacency or distribution

--- mixingAssortativity.m
+++ mixingAssortativity.m
@@ -1,8 +1,15 @@
-function coeff = mixingAssortativity(M)
-% coeff = mixingAssortativity(M)
-% returns assortativity coefficient for distribution matrix M
+function coeff = mixingAssortativity(A, S)
+% coeff = mixingAssortativity(A, S)
+% returns assortativity coefficient for distribution matrix A
+% if S is given, assume A is adjacency matrix
 
-    e = M / sum(M(:)); % normalize distribution
+    if exist('S', 'var')
+        m = length(S);
+        [from, to] = find(A);
+        A = accumarray([S(from) S(to)], 1, [m m]);
+    end
+
+    e = A / sum(A(:)); % normalize distribution
     a = sum(e, 2);     % from
     b = sum(e);        % to
     coeff = (sum(diag(e)) - b * a) / (1 - b * a);
