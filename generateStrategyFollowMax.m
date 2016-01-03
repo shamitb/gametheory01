e8cfd92a015ef6b020af62f455fb311e5c0953ff
Shamit Bagchi <shamit@gmail.com>
2016-01-03 17:31:55 +0100

    Update generateStrategyFollowMax.m

--- generateStrategyFollowMax.m
+++ generateStrategyFollowMax.m
@@ -7,8 +7,22 @@ handle = @strategyFollowMax;
         
         % Get the max connection
         maxi = max(U);
-        I = find( maxi == U );
-        connection = I(randperm(size(I,2), 1));
+        I = find( maxi == U )
+        
+        index = randperm(size(I,2), 1);
+        connection = I(index)
+        
+        while(length(I) > 1)
+            
+            if A(agent, connection) == 1
+                I(index) = [];
+                index = randperm(size(I,2), 1);
+                connection = I(index)
+            else
+                break;
+            end
+        end
+        
         
         newA = [];
         newpL = [];
