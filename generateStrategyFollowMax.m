9e58fe2c924d63d851c878af86b7b9b6578ae312
Shamit Bagchi <shamit@gmail.com>
2016-01-04 14:57:18 +0100

    Create generateStrategyFollowMax.m

---
+++ generateStrategyFollowMax.m
@@ -0,0 +1,36 @@
+function handle = generateStrategyFollowMax(beta, cost)
+
+handle = @strategyFollowMax;
+
+    function [connection, newA, newpL, newU] = strategyFollowMax(agent, A, pL, U)
+        % Always make best move for self
+        
+        % Get the max connection
+        maxi = max(U);
+        I = find( maxi == U )
+        
+        index = randperm(size(I,2), 1);
+        connection = I(index)
+        
+        while(length(I) > 1)
+            
+            if A(agent, connection) == 1
+                I(index) = [];
+                index = randperm(size(I,2), 1);
+                connection = I(index)
+            else
+                break;
+            end
+        end
+        
+        if A(agent, connection) == 1
+            connection = agent;
+        end
+        
+        newA = [];
+        newpL = [];
+        newU = [];
+        
+    end
+
+end
