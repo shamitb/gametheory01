0181e3e1ae55073c5dd0c9463b8fcbc251e5f90d
Bjorn Persson Mattsson <bjorn.pm@plankter.se>
2016-01-13 21:09:42 +0100

    Added some figure code

--- test.m
+++ test.m
@@ -1,4 +1,4 @@
-N = 256;
+N = 20;
 A = initialAction(N);
 mistakeRate = 0.5 / N;
 b = 1;
@@ -8,6 +8,7 @@ for node = 1:N-1
     A(node, node + 1) = 1;
 end
 A(N, 1) = 1;
+A = initialAction(N, 0.1);
 
 [U, pL] = utility(A, [], b, c);
 
@@ -23,11 +24,21 @@ strategy{4} = generateStrategyCooperative(b, c);
 strategy{5} = generateStrategyThreshold(strategy{2}, strategy{3}, 0.25);
 
 %S = sort(randi(3, N, 1) + 1);
-S = ones(N, 1) * 2;
+S = ones(N, 1) * 4;
 
 duration = N * 200;
 
 fullStats = true;
 
-[newA, newpL, newU, statistics]...
-    = iterateGame(S, A, pL, U, duration, strategy, mistakeRate, fullStats);
+figure;
+for i=1:duration
+    fprintf('Iteration %i \n',i);
+    [newA, newpL, newU, statistics]...
+        = iterateGame(S, A, pL, U, 2, strategy, mistakeRate, fullStats);
+    plot(digraph(newA));
+    A = newA;
+    pL = newpL;
+    U = newU;
+    drawnow;
+end
+%[newA, newpL, newU, statistics] = iterateGame(S, A, pL, U, duration, strategy, mistakeRate, fullStats);
